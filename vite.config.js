81dc182401977bb262c744e3d1ad533ba2b69aad
coderdivya18 <coderdivya18@gmail.com>
2025-06-18 12:18:31 +0530

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: "/Youtube_clone/",
+  build: {
+    rollupOptions: {
+      input: "/index.html",
+    },
+  },
 });
